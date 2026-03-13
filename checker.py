9938ae6cb57ef3488c702368d3463e854021c892
Kumar-Aryan <60267891+Kumar-Aryan@users.noreply.github.com>
2026-03-13 12:50:43 +0530

    Refactor checker.py to send test alert
    Removed movie booking check and notification logic. Added a test alert message.

--- checker.py
+++ checker.py
@@ -1,14 +1,8 @@
 import requests
 import os
-import json
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
-
-MOVIE_URL = "https://in.bookmyshow.com/bengaluru/movies/dhurandhar-the-revenge"
-
-STATE_FILE = "state.json"
-
 
 def send_alert(message):
 
@@ -22,51 +16,6 @@ def send_alert(message):
         }
     )
 
+print("Script started")
 
-def already_notified():
-
-    try:
-        with open(STATE_FILE) as f:
-            data = json.load(f)
-            return data.get("notified", False)
-
-    except:
-        return False
-
-
-def mark_notified():
-
-    with open(STATE_FILE, "w") as f:
-        json.dump({"notified": True}, f)
-
-
-def check_booking():
-
-    response = requests.get(MOVIE_URL)
-
-    page = response.text.lower()
-    if True:
-
-    # if "mar 19" in page or "19 mar" in page:
-
-        if "pm" in page:
-
-            if not already_notified():
-
-                message = """
-🎬 DHURANDAR BOOKINGS OPEN
-
-City: Bangalore
-Date: 19 March
-Time: After 12 PM
-
-Book immediately:
-https://in.bookmyshow.com/bengaluru/movies/dhurandhar-the-revenge
-"""
-
-                send_alert(message)
-
-                mark_notified()
-
-
-check_booking()
+send_alert("✅ Test alert from GitHub Actions bot")
